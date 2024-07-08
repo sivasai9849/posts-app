5b42eeb55074c341d9e1a95d9f1de09b2231c7de
sivasai9849 <109414914+sivasai9849@users.noreply.github.com>
2024-07-08 16:54:56 +0530

    added files

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer, DefaultTheme, useNavigation, useRoute } from '@react-navigation/native';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { SafeAreaView, TouchableOpacity, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Login from './src/pages/Login';
+import Signup from './src/pages/Signup';
+import Home from './src/pages/Home';
+import ForgotPassword from './src/pages/ForgotPassword';
+import EditProfile from './src/pages/Editprofile';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white',
+  },
+};
+
+const BackButton = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const canGoBack = navigation.canGoBack();
+
+  if (!canGoBack || route.name === 'Login') {
+    return null;
+  }
+
+  const handlePress = () => {
+    navigation.goBack();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <TouchableOpacity style={{ marginLeft: 10 }} onPress={handlePress}>
+      <Ionicons name="arrow-back" size={24} color="black" />
+    </TouchableOpacity>
+  );
+};
+
+const App = () => {
+  return (
+    <View className='flex-1 '>
+    <NavigationContainer theme={MyTheme}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={({ route }) => ({
+            headerStyle: {
+              backgroundColor: 'white',
+            },
+            headerTintColor: 'black',
+            headerTitleAlign: 'center',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerLeft: () => <BackButton />,
+            headerShown: route.name == 'Signup', 
+            headerTitle: '',
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          })}
+        >
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="EditProfile" component={EditProfile} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
